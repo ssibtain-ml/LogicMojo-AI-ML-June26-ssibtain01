9b6039c05d4684a935eaa1b9c983b729593b46b3
ssibtai-ml <sibbi.sam@gmail.com>
2026-06-21 12:13:40 -0500

    expense calc

--- assignment/helloWorld.py
+++ assignment/helloWorld.py
@@ -4,5 +4,18 @@ a = int(input("Rent: "))
 b = int(input("Groceries: "))
 c = int(input("Utilities: "))
 d = int(input("Commute: "))
-print("Total Expense" + str(a+b+c+d))
+print("Total Expense: " + str(a+b+c+d))
 
+strName = input("Emp Name?: ")
+intScore = int(input("Emp Score?: "))
+
+if intScore < 33:
+    print("sorry you failed")
+elif intScore >= 33 and intScore <= 60:
+    print("satisfactory")
+elif intScore > 60 and intScore <= 70:
+    print("good")
+elif intScore > 70 and intScore <= 100:
+    print("excellent")
+else:
+    print("invalid input")
